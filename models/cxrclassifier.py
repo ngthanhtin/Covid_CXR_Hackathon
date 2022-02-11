b33ee42b03078363ed3a3700aa1b7f3f3f679119
nguyen_thanh_tin <thanhtinqn0903039105@gmail.com>
2022-02-11 11:15:06 +0900

    add model for training metadata with cnn

--- models/cxrclassifier.py
+++ models/cxrclassifier.py
@@ -3,6 +3,7 @@ import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
+import timm
 
 def _find_index(ds, desired_label):
     desired_index = None
@@ -20,9 +21,13 @@ class CXRClassifier(nn.Module):
         super(CXRClassifier, self).__init__()
 
         self.backbone = models.densenet121(pretrained=True)
+        # self.backbone = models.resnet50(pretrained=True)
         in_feature = self.backbone.classifier.in_features
+        # in_feature = self.backbone.fc.in_features
         self.backbone.classifier = torch.nn.Sequential(
                 torch.nn.Linear(in_feature, n_labels))
+        # self.backbone.fc = torch.nn.Sequential(
+        #         torch.nn.Linear(in_feature, n_labels))
 
     def forward(self, x):
         x = self.backbone(x)
---
+++ models/metadamodel.py
@@ -0,0 +1,66 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+sigmoid = nn.Sigmoid()
+
+
+class Swish(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, i):
+        result = i * sigmoid(i)
+        ctx.save_for_backward(i)
+        return result
+    @staticmethod
+    def backward(ctx, grad_output):
+        i = ctx.saved_variables[0]
+        sigmoid_i = sigmoid(i)
+        return grad_output * (sigmoid_i * (1 + i * (1 - sigmoid_i)))
+
+
+class Swish_Module(nn.Module):
+    def forward(self, x):
+        return Swish.apply(x)
+
+
+class MetaCXR_Classifier(nn.Module):
+    def __init__(self, n_labels, n_meta_features=0, n_meta_dim=[512, 128]):
+        super(MetaCXR_Classifier, self).__init__()
+        self.n_meta_features = n_meta_features
+        self.backbone = models.densenet121(pretrained=True)
+        self.dropouts = nn.ModuleList([
+            nn.Dropout(0.5) for _ in range(5)
+        ])
+        in_ch = self.enet.classifier.in_features
+        if n_meta_features > 0:
+            self.meta = nn.Sequential(
+                nn.Linear(n_meta_features, n_meta_dim[0]),
+                nn.BatchNorm1d(n_meta_dim[0]),
+                Swish_Module(),
+                nn.Dropout(p=0.3),
+                nn.Linear(n_meta_dim[0], n_meta_dim[1]),
+                nn.BatchNorm1d(n_meta_dim[1]),
+                Swish_Module(),
+            )
+            in_ch += n_meta_dim[1]
+        self.myfc = nn.Linear(in_ch, n_labels)
+        # self.enet.classifier = nn.Identity()
+
+    def extract(self, x):
+        x = self.backbone(x)
+        return x
+
+    def forward(self, x, x_meta=None):
+        x = self.extract(x).squeeze(-1).squeeze(-1)
+        if self.n_meta_features > 0:
+            x_meta = self.meta(x_meta)
+            x = torch.cat((x, x_meta), dim=1)
+        for i, dropout in enumerate(self.dropouts):
+            if i == 0:
+                out = self.myfc(dropout(x))
+            else:
+                out += self.myfc(dropout(x))
+        out /= len(self.dropouts)
+        return out
